e813b18ae65cf4b72ef9db4482d4948b5177bcdb
Wali ullah <wali190939@gmail.com>
2020-08-24 09:18:11 +0600

    initial commit

---
+++ resources/js/current-videos.js
@@ -0,0 +1,112 @@
+$(".over1").on({
+  mouseenter: function () {
+    $(".image1").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image1").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over2").on({
+  mouseenter: function () {
+    $(".image2").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image2").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over3").on({
+  mouseenter: function () {
+    $(".image3").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image3").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over4").on({
+  mouseenter: function () {
+    $(".image4").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image4").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over5").on({
+  mouseenter: function () {
+    $(".image5").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image5").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over6").on({
+  mouseenter: function () {
+    $(".image6").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image6").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over7").on({
+  mouseenter: function () {
+    $(".image7").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image7").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
+$(".over8").on({
+  mouseenter: function () {
+    $(".image8").css({
+      transform: "scale(1.2)",
+      backgroundColor: "gray",
+    });
+  },
+  mouseleave: function () {
+    $(".image8").css({
+      transform: "scale(1)",
+      backgroundColor: "transparent",
+    });
+  },
+});
